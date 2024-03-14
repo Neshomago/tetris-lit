cb188ce2c56a6159c4535f7dedf84c5fc51e2ff9
Andres Torres <magoyde@gmail.com>
2024-03-13 19:13:44 -0500

    feat(utils.js): se agrega archivo de utils

---
+++ src/utils.js
@@ -0,0 +1,11 @@
+export const EMPTY = 0;
+export const COLORS = ["#000000", "#FF0000", "#00FF00", "#0000FF", "#FFFF00", "#FF00FF", "#00FFFF", "#FFFFFF"];
+export const pieces = [
+            { shape: [[1, 1, 1, 1]], color: 1 },
+            { shape: [[1, 1, 1], [1, 0, 0]], color: 2 },
+            { shape: [[1, 1, 1], [0, 0, 1]], color: 3 },
+            { shape: [[1, 1, 0], [0, 1, 1]], color: 4 },
+            { shape: [[1, 1, 1], [0, 1, 0]], color: 5 },
+            { shape: [[1, 1, 1], [1, 1, 1]], color: 6 },
+            { shape: [[1, 1], [1, 1]], color: 7 },
+        ];
